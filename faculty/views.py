43f3e54e53879f1757319dadf30c06f267407e7d
Shivansh Tiwari <shivanshtiwari6849@gmail.com>
2025-08-16 23:35:33 +0530

    Refactor teacher dashboard: add login requirement, improve data retrieval, and enhance UI layout

--- faculty/views.py
+++ faculty/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib import messages
 from .forms import TeacherWithUserForm  # Use the correct form
-
+from faculty.models import Teacher
+from django.contrib.auth.decorators import login_required
 def register_teacher(request):
     if request.method == 'POST':
         form = TeacherWithUserForm(request.POST)
@@ -12,6 +13,7 @@ def register_teacher(request):
     else:
         form = TeacherWithUserForm()
     return render(request, 'faculty/register_teacher.html', {'form': form})
-
+@login_required
 def teacher_dashboard(request):
-    return render(request, 'faculty/teacher_dashboard.html')
+    teacher = get_object_or_404(Teacher, user=request.user)
+    return render(request, 'faculty/teacher_dashboard.html', {'teacher': teacher})
