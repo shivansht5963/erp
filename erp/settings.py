2ca8b0425b46e8986d16385c8148249f37e27a8b
Shivansh Tiwari <shivanshtiwari6849@gmail.com>
2025-08-31 10:50:33 +0530

    Add WhiteNoise middleware for serving static files in production

--- erp/settings.py
+++ erp/settings.py
@@ -139,6 +139,11 @@ LOGIN_REDIRECT_URL = 'dashboard_redirect' # We will create a 'home' URL next.
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Enable WhiteNoise for static files
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
